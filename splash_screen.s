a0a798a524feb7434e9eace489c2b3e0545770f7
Sam Bonnekamp <sam@bonnekamp.net>
2026-01-12 02:22:18 +0800

    added screen output

--- splash_screen.s
+++ splash_screen.s
@@ -16,18 +16,31 @@ counter = $00                   ;location of the counter
 last_toggle = $03
 
         .org $8000
+
+message: .asciiz "samux kernel :3"
 _start:
         lda #$0                 ;init counter
         sta counter
         sta counter+$1
         sta counter+$2
         sta last_toggle
-        
+
         jsr init_ports
 
+        jsr init_screen
+
         jsr init_timer
-        
+
         cli
+
+        ldx #$0
+print_loop:
+        lda message, x
+        beq _loop
+        jsr print_char
+        inx
+        jmp print_loop
+
 _loop:
         jsr toggle_led
         jmp _loop
@@ -45,13 +58,13 @@ toggle_led:
         sta last_toggle
 end_toggle:
         rts
-        
+
 ;;a, sreg
 init_ports:
         lda #$ff                ;all pints port b output
         sta DDRB
 
-        lda #%11111111          ;top 3 pins, bottom pin port a to output 
+        lda #%11111111          ;top 3 pins, bottom pin port a to output
         sta DDRA
 
         lda #$0                 ;init both ports to pull down
@@ -77,6 +90,73 @@ init_timer:
         pla
         rts
 
+init_screen:
+        lda #%00000001          ;clear display
+        sta PORTB
+        jsr lcd_instruction_send
+
+        lda #%00111000          ;function set (set 8-bit mode, 2 line displayt, 5x8 font)
+        sta PORTB
+        jsr lcd_instruction_send
+
+        lda #0                  ;set control pins to 0
+        sta PORTA
+        jsr lcd_instruction_send
+
+        lda #%00001110          ;display on, cur on, blink off
+        sta PORTB
+        jsr lcd_instruction_send
+
+        lda #%00000110          ;entry mode, incr, no scroll
+        sta PORTB
+        jsr lcd_instruction_send
+        rts
+
+;; sreg
+lcd_wait:
+        pha
+        lda #0                  ;set drrb to input to read busy flag
+        sta DDRB
+reread:
+        lda #RW
+        sta PORTA
+        lda #(E|RW)             ;compile-time constant eval I think
+        sta PORTA
+        lda PORTB               ;read busy flag
+        and #%10000000          ;compare with top bit (bf)
+        bne reread
+
+        lda #RW
+        sta PORTA
+
+        lda #$ff                ;reset ddrb to output
+        sta DDRB
+        pla
+        rts
+
+
+;; changes a
+lcd_instruction_send:
+        jsr lcd_wait
+        lda #E                  ;toggle enable
+        sta PORTA
+        lda #0
+        sta PORTA
+        rts
+
+;; changes port a
+print_char:
+        jsr lcd_wait
+        sta PORTB
+        lda #RS                  ;turn on RS
+        sta PORTA
+
+        lda #(RS | E)            ;toggle enable and RS
+        sta PORTA
+        lda #RS                 ;un toggle enable but keep RS
+        sta PORTA
+        rts
+
 _nmi:
         rti
 _irq:
@@ -90,10 +170,9 @@ _irq:
 exit_irq:
         pla
         rti
-        
+
 ;; jump table
         .org $FFFA
         .word _nmi
         .word _start
         .word _irq
-        
